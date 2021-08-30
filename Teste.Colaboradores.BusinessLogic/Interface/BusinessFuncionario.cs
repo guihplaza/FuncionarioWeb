e7c14242502bf150e33611453790b1f122bf3e34
Guilherme Plaza <guihplaza@gmail.com>
2021-08-30 14:02:57 -0300

    SEPARAÇÃO DE CAMADAS

---
+++ Teste.Colaboradores.BusinessLogic/Interface/BusinessFuncionario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste.Colaboradores.BusinessLogic.Interface
+{
+    public class BusinessFuncionario
+    {
+        public async Task<IActionResult> DespesasSalColaborador()
+        {
+            var funcionario = new Funcionario();
+            funcionario.TotalSalarios = await _context.Funcionarios.OrderByDescending(o => o.Salario).ToListAsync();
+            ViewBag.TotalSalarios = string.Format("{0:c}", funcionario.TotalSalarios.Sum(w => w.Salario));
+
+        }
+    }
+}
---
+++ Teste.Colaboradores.Domain/Funcionario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FUNCIONARIOS.Models
+{
+    public class Funcionario
+	{
+        [Key]
+        public int IdFuncionario { get; set; }
+        
+        [Required(ErrorMessage = "O campo Valor deve ser preenchido")]
+        public string Nome { get; set; }
+        public string Sexo { get; set; }
+
+        public int Pis { get; set; }
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "O campo Valor deve ser preenchido")]
+        [DataType(DataType.Currency)]
+        public Decimal Salario { get; set; }
+
+        public string Email { get; set; }
+        public DateTime Data_admissao { get; set; }
+
+        [NotMapped]
+        public IEnumerable<Funcionario> TotalSalarios { get; set; }
+
+        public static List<Funcionario> GetSexos()
+        {
+            var listaSexo = new List<Funcionario>()
+            {
+                new Funcionario(){  Sexo="Masculino"},
+                new Funcionario(){  Sexo="Feminino"},
+                new Funcionario(){  Sexo="Outros"},
+                
+            };
+            return listaSexo;
+        }
+    }
+}
